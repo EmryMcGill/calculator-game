7a5a596f7984b33aa4a84c4f0dbfe68125dddfc6
Panda_42 <emrymcgill@gmail.com>
2018-07-29 10:37:18 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from math import*
+
+
+
+class calculator:
+
+    def option(option):
+        option = input("Enter a math symbol. Ex: + - * /    :")
+
+    def addition(x,y):
+        x = input("Enter a number")
+        y = input("Enter another number")
+        added = float(x)+float(y)
+        print(added)
+
+    def subtraction(x,y):
+        x = input("Enter a number")
+        y = input("Enter another number")
+        sub = float(x)-float(y)
+        print(sub)
+
+    def multiplication(x,y):
+        x = input("Enter a number")
+        y = input("Enter another number")
+        mult = float(x)*float(y)
+        print(mult)
+
+    def division(x,y):
+        x = input("Enter a number")
+        y = input("Enter another number")
+        div = float(x)/float(y)
+        print(div)
+
+
+
+
+
+
+
