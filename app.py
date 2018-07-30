c9475ff100ca711586d1480026436fb218f2efb4
Panda_42 <emrymcgill@gmail.com>
2018-07-29 21:31:07 -0700

    Has a age verification now

--- app.py
+++ app.py
@@ -18,18 +18,20 @@ class calculator:
         if option == "/":
             cal.division(1)
             cal.restart()
+
+
+
     def addition(x,y):
         x = input("Enter a number")
         y = input("Enter another number")
         added = float(x)+float(y)
-        print(added)
-
+        print("Your answer is: " + str(added))
 
     def subtraction(x,y):
         x = input("Enter a number")
         y = input("Enter another number")
         sub = float(x)-float(y)
-        print(sub)
+        print("Your answer is: " + str(sub))
 
     def multiplication(x,y):
         x = input("Enter a number")
@@ -42,6 +44,23 @@ class calculator:
         y = input("Enter another number")
         div = float(x)/float(y)
         print(div)
+
+
+
+
+
+
+name=input("Enter your name: ")
+ageStr=input("Hello, " + name + " enter your age: ")
+age=float(ageStr)
+
+if age<21:
+    print("Sorry, " + " but you are not old enough to access the calculator.")
+    exit()
+else:
+    print("You have access to the calculator.")
+
+
 
 cal = calculator()
 cal.restart()
